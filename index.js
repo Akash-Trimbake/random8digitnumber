d62828583848d214c1ffdc7108176cac9ede2dd3
akash <trimbakeakash5@gmail.com>
2024-01-25 09:13:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Define a route for the root path
+app.get("/", (req, res) => {
+  const min = 100;
+  const max = 99999999;
+
+  const random8DigitNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const formattedNumber = String(random8DigitNumber).padStart(8, "0");
+  res.send(`${formattedNumber}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
